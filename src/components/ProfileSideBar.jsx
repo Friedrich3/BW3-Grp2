e297bedce7f7d4ec37b34227a4d8f90caea53f62
WalterX95 <walter_1995@hotmail.it>
2025-01-27 22:03:39 +0100

    Creato ProfileCardsSidebar.jsx

--- src/components/ProfileSideBar.jsx
+++ src/components/ProfileSideBar.jsx
@@ -1,8 +1,10 @@
+import ProfileCardsSideBar from "./ProfileCardsSideBar";
+
 const ProfileSideBar = function (){
     
     return(
         <>
-        
+           <ProfileCardsSideBar/>
         </>
     )
 
