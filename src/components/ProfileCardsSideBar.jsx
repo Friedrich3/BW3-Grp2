76ed01606298f3a2af39b66c4fdfa7840f31a711
Federico Tonti <148573975+Friedrich3@users.noreply.github.com>
2025-01-28 09:40:08 +0100

    commit fix dispatch

--- src/components/ProfileCardsSideBar.jsx
+++ src/components/ProfileCardsSideBar.jsx
@@ -1,17 +1,16 @@
 import { Card } from "react-bootstrap";
 //import { getDataAction } from "../redux/action";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { useEffect } from "react";
 
 const ProfileCardsSideBar = () => {
-  const dispatch = useDispatch();
+
   const profilo = useSelector((state) => {
     return state.profile.data;
   });
 
   useEffect(() => {
-    // dispatch(getDataAction());
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+
   }, []);
 
   return (
