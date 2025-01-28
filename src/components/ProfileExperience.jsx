be317596c2a5740690bda7a3644191eb5ba9ec0e
Federico Tonti <148573975+Friedrich3@users.noreply.github.com>
2025-01-28 11:54:12 +0100

    scheletro experience page

--- src/components/ProfileExperience.jsx
+++ src/components/ProfileExperience.jsx
@@ -1,24 +1,89 @@
-import { Card, Col, Container, Row } from "react-bootstrap"
+import { Card, Col, Container, ListGroup, Row } from "react-bootstrap";
 
-const ProfileExperience = function(){
-    return(
-        <>
-        <Container fluid className="mt-2">
-            <Row>
-                <Col className="p-0">
-                <Card>
+const ProfileExperience = function () {
+  return (
+    <>
+      <Container fluid className="mt-2">
+        <Row>
+          <Col className="p-0">
+            <Card>
               <Card.Body>
                 <Card.Title>Esperienza</Card.Title>
-                <Card.Text>
+                <ListGroup>
+                  {/* IL PRIMO LIST ITEM */}
+                  <ListGroup.Item className="border-0 border-bottom container-fluid">
+                    <Row>
+                      <Col xs={1}>
+                        <img
+                          src="https://placecats.com/50/50"
+                          alt=""
+                          className="rounded-circle"
+                        />
+                      </Col>
+                      <Col xs={10} className="ms-2">
+                        <div>
+                          <h6 className="m-0">Company</h6>
+                          <small>Quanto tempo</small>
+                        </div>
+                        <div className="mt-3">
+                          <h6>Ruolo interno</h6>
+                          <small className=" text-secondary">
+                            Da quando - Presente - Per Quanto
+                          </small>
+                          {/* TODO INSERIRE FUNZIONE CHE PRENDA SOLO MESE E ANNO , A QUANDO SE NULL RESTITUISCE PRESENTE - Per quanto usa o la data presente nell api oppure la data di oggi se null*/}
+                          <br />
+                          <small className=" text-secondary">Area</small>
+                          <p className="mt-2">
+                            Descrizione : Lorem ipsum dolor sit amet,
+                            consectetur adipisicing elit. Perferendis molestiae
+                            laborum, ipsa nihil cum illo ullam tenetur officia
+                            consequatur aliquam dolor nobis similique quia
+                            sapiente, minus ipsum earum optio quae!
+                          </p>
+                        </div>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+
+                  {/* TUTTI GLI ALTRI */}
+                  <ListGroup.Item className="border-0 border-bottom container-fluid">
+                    <Row>
+                      <Col xs={1}>
+                        <img
+                          src="https://placecats.com/50/50"
+                          alt=""
+                          className="rounded-circle"
+                        />
+                      </Col>
+                      <Col xs={10} className="ms-2">
+                        <div className="d-flex flex-column">
+                          <h6>Ruolo interno</h6>
+                          <span>Company name</span>
+                          <small className="text-secondary">
+                            Da quando - A quando - Per quanto{" "}
+                          </small>
+                          <small className="text-secondary">Area</small>
+                          <p className="mt-2">
+                            Descrizione : Lorem ipsum dolor sit amet,
+                            consectetur adipisicing elit. Perferendis molestiae
+                            laborum, ipsa nihil cum illo ullam tenetur officia
+                            consequatur aliquam dolor nobis similique quia
+                            sapiente, minus ipsum earum optio quae!
+                          </p>
+                        </div>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+
+
                   
-                </Card.Text>
+                </ListGroup>
               </Card.Body>
             </Card>
-                </Col>
-            </Row>
-        </Container>
-        </>
-    )
-
-}
-export default ProfileExperience
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+export default ProfileExperience;
