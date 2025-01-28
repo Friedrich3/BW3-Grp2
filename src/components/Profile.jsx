b3edc3417b565e422dc7e40b3a430cb3c200ce4d
Federico Tonti <148573975+Friedrich3@users.noreply.github.com>
2025-01-28 09:32:20 +0100

    commit pre merge

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -10,7 +10,7 @@ const Profile = function(){
             <Col xs={12} md={9} className="border border-black">
                 <ProfileMain />              {/*      QUI ANDRANNO TUTTI I COMPONENTI DELLA SEZIONE PRINCIPALE QUINDI: BANNERPROFILO , ATTIVITA, ESPERIENZE, FORMAZIONE , COMPETENZE ecc..      */}
             </Col>
-            <Col xs={12} md={2} className="border border-black">
+            <Col xs={12} md={3} className="border border-black">
                 <ProfileSideBar />              {/*      QUI ANDRANNO TUTTI I COMPONENTI DELLA SEZIONE SECONDARIA QUINDI: altre schifezze ,'PERSONE CHE VORRESTI CONOSCERE' ,     */}
             </Col>
 
--- src/components/ProfileCardsSideBar.jsx
+++ src/components/ProfileCardsSideBar.jsx
@@ -1,33 +1,39 @@
 import { Card } from "react-bootstrap";
-import { getDataAction } from "../redux/action";
+//import { getDataAction } from "../redux/action";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 
 const ProfileCardsSideBar = () => {
-   const dispatch = useDispatch()
-   const profilo = useSelector((state)=>{ return state.profile.data})
-    
-     useEffect(()=>{
-                dispatch(getDataAction()) 
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        },[])
+  const dispatch = useDispatch();
+  const profilo = useSelector((state) => {
+    return state.profile.data;
+  });
 
-    return (
-        <>
-          <Card style={{ width: '18rem' }}>
-                <Card.Body>
-                    <Card.Title>Lingua del Profilo</Card.Title>
-                    {/*INSERIRE FETCH LINGUA DEL PROFILO SOPRA AL RETURN */}
-                    <Card.Subtitle className="mb-2 text-muted">{profilo.email}</Card.Subtitle>
-                    <hr></hr>
-                    <Card.Title>Profilo Pubblico e URL</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">{profilo.name}</Card.Subtitle>
-                </Card.Body>
-            </Card>
-         
-        </>
-    )
+  useEffect(() => {
+    // dispatch(getDataAction());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
+  return (
+    <>
+      {profilo && (
+        <Card >
+          <Card.Body>
+            <Card.Title>Lingua del Profilo</Card.Title>
+            {/*INSERIRE FETCH LINGUA DEL PROFILO SOPRA AL RETURN */}
+            <Card.Subtitle className="mb-2 text-muted">
+              {profilo.email}
+            </Card.Subtitle>
+            <hr></hr>
+            <Card.Title>Profilo Pubblico e URL</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">
+              {profilo.name}
+            </Card.Subtitle>
+          </Card.Body>
+        </Card>
+      )}
+    </>
+  );
 };
 
 export default ProfileCardsSideBar;
