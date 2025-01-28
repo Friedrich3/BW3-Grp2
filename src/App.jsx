fb3952315809ff1ddbeea967176fedd53c58800e
Federico Tonti <148573975+Friedrich3@users.noreply.github.com>
2025-01-28 14:32:34 +0100

    commit post merge

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import "./App.css";
 import Profile from "./components/Profile";
 import NavbarLinkedin from "./components/NavbarLinkedin";
 import NotFound from "./components/NotFound";
-import FooterMini from "./components/FooterMini"
 import FooterLinkedin from "./components/FooterLinkedin";
 
 function App() {
@@ -16,7 +15,6 @@ function App() {
 
         <Route path="*" element={<NotFound />} />
       </Routes>
-      <FooterMini />
       <FooterLinkedin />
     </BrowserRouter>
   );
