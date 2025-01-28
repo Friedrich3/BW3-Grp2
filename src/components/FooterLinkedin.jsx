27041740381c145258f88878f7be94dea6d61cf9
migueljano <angel1999dtb@gmail.com>
2025-01-28 13:04:11 +0100

    modifiche footer Responsive

--- src/components/FooterLinkedin.jsx
+++ src/components/FooterLinkedin.jsx
@@ -1,3 +1,4 @@
+import { Col, Row } from "react-bootstrap";
 import {
   GearFill,
   QuestionCircleFill,
@@ -6,9 +7,11 @@ import {
 
 const FooterLinkedin = function () {
   return (
-    <>
-      <footer>
-        <div className="container py-5">
+
+    <footer className="container py-5">
+      <Row >
+        <Col md={12} lg={6}>
+
           <div className="row">
             {/* prima colonna con lista di link vuoti */}
             <div className="col">
@@ -88,107 +91,113 @@ const FooterLinkedin = function () {
                 </li>
               </ul>
             </div>
+          </div>
 
-            {/* seconda colonna con icone e option */}
-            <div className="col d-flex flex-column flex-md-row gap-3">
-              <ul className="list-unstyled d-flex flex-column gap-1 w-100">
-                <li>
-                  <div className="d-flex gap-3">
-                    <QuestionCircleFill className="text-dark fs-4" />
-                    <div className="text-secondary">
-                      <a
-                        href="#"
-                        className="text-decoration-none text-secondary"
-                      >
-                        <p
-                          className="m-0 fw-bold custom-hover"
-                          style={{ fontSize: "14px" }}
-                        >
-                          Domande?
-                        </p>
-                      </a>
-                      <p className="text-muted" style={{ fontSize: "12px" }}>
-                        Visita il nostro Centro assistenza.
-                      </p>
-                    </div>
-                  </div>
-                </li>
-                <li>
-                  <div className="d-flex gap-3">
-                    <GearFill className="text-dark fs-4" />
-                    <div className="text-secondary">
-                      <a
-                        href="#"
-                        className="text-decoration-none text-secondary"
-                      >
-                        <p
-                          className="m-0 fw-bold custom-hover"
-                          style={{ fontSize: "14px" }}
-                        >
-                          Gestisci il tuo account e la tua privacy
-                        </p>
-                      </a>
-                      <p className="text-muted" style={{ fontSize: "12px" }}>
-                        Vai alle impostazioni
-                      </p>
-                    </div>
-                  </div>
-                </li>
-                <li>
-                  <div className="d-flex gap-3">
-                    <ShieldShaded className="text-dark fs-4" />
-                    <div className="text-secondary">
-                      <a
-                        href="#"
-                        className="text-decoration-none text-secondary"
-                      >
-                        <p
-                          className="m-0 fw-bold custom-hover"
-                          style={{ fontSize: "14px" }}
-                        >
-                          Trasparenza sui contenuti consigliati
-                        </p>
-                      </a>
-                      <p className="text-muted" style={{ fontSize: "12px" }}>
-                        Scopri di più sui contenuti consigliati.
-                      </p>
-                    </div>
-                  </div>
-                </li>
-              </ul>
+        </Col>
 
-              <div className="dropdown w-100 d-flex justify-content-start justify-content-md-start align-items-start">
-                <div>
-                  <p
-                    className="m-0 text-secondary"
-                    style={{ fontSize: "12px" }}
-                  >
-                    Seleziona lingua
-                  </p>
-                  <select
-                    className="form-select text-secondary fw-bold"
-                    style={{ fontSize: "12px", width: "200px" }}
-                    defaultValue={"it"}
-                  >
-                    <option value="it">Italiano</option>
-                    <option value="en">Inglese</option>
-                    <option value="es">Spagnolo</option>
-                    <option value="fr">Francese</option>
-                  </select>
+        <Col md={12} lg={6}>
+          {/* seconda colonna con icone e option */}
+          <div className="col d-flex flex-column flex-md-row gap-3">
+            <ul className="list-unstyled d-flex flex-column gap-1 w-100">
+              <li>
+                <div className="d-flex gap-3">
+                  <QuestionCircleFill className="text-dark fs-4" />
+                  <div className="text-secondary">
+                    <a
+                      href="#"
+                      className="text-decoration-none text-secondary"
+                    >
+                      <p
+                        className="m-0 fw-bold custom-hover"
+                        style={{ fontSize: "14px" }}
+                      >
+                        Domande?
+                      </p>
+                    </a>
+                    <p className="text-muted" style={{ fontSize: "12px" }}>
+                      Visita il nostro Centro assistenza.
+                    </p>
+                  </div>
                 </div>
+              </li>
+              <li>
+                <div className="d-flex gap-3">
+                  <GearFill className="text-dark fs-4" />
+                  <div className="text-secondary">
+                    <a
+                      href="#"
+                      className="text-decoration-none text-secondary"
+                    >
+                      <p
+                        className="m-0 fw-bold custom-hover"
+                        style={{ fontSize: "14px" }}
+                      >
+                        Gestisci il tuo account e la tua privacy
+                      </p>
+                    </a>
+                    <p className="text-muted" style={{ fontSize: "12px" }}>
+                      Vai alle impostazioni
+                    </p>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <div className="d-flex gap-3">
+                  <ShieldShaded className="text-dark fs-4" />
+                  <div className="text-secondary">
+                    <a
+                      href="#"
+                      className="text-decoration-none text-secondary"
+                    >
+                      <p
+                        className="m-0 fw-bold custom-hover"
+                        style={{ fontSize: "14px" }}
+                      >
+                        Trasparenza sui contenuti consigliati
+                      </p>
+                    </a>
+                    <p className="text-muted" style={{ fontSize: "12px" }}>
+                      Scopri di più sui contenuti consigliati.
+                    </p>
+                  </div>
+                </div>
+              </li>
+            </ul>
+
+            <div className="dropdown w-100 d-flex justify-content-start justify-content-md-start align-items-start">
+              <div>
+                <p
+                  className="m-0 text-secondary"
+                  style={{ fontSize: "12px" }}
+                >
+                  Seleziona lingua
+                </p>
+                <select
+                  className="form-select text-secondary fw-bold"
+                  style={{ fontSize: "12px", width: "200px" }}
+                  defaultValue={"it"}
+                >
+                  <option value="it">Italiano</option>
+                  <option value="en">Inglese</option>
+                  <option value="es">Spagnolo</option>
+                  <option value="fr">Francese</option>
+                </select>
               </div>
             </div>
           </div>
 
-          {/* copyright */}
-          <div className="row mt-3 pt-2">
-            <p className="mb-0 text-secondary" style={{ fontSize: "12px" }}>
-              LinkedIn Corporation © 2025{" "}
-            </p>
-          </div>
-        </div>
-      </footer>
-    </>
+
+        </Col>
+
+      </Row>
+      {/* copyright */}
+      <div className="row mt-3 pt-2">
+        <p className="mb-0 text-secondary" style={{ fontSize: "12px" }}>
+          LinkedIn Corporation &copy; 2025{" "}
+        </p>
+      </div>
+    </footer >
+
   );
 };
 
