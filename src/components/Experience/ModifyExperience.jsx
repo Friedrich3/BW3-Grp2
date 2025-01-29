68221ba4142122a5b2c6dcb9cef3c96c76a1f7f4
Federico Tonti <148573975+Friedrich3@users.noreply.github.com>
2025-01-29 12:30:31 +0100

    aggiunta esperienza dentro pagina modifica

--- src/components/Experience/ModifyExperience.jsx
+++ src/components/Experience/ModifyExperience.jsx
@@ -1,6 +1,9 @@
-import { Col, Container, ListGroup, Row } from "react-bootstrap";
+import { Alert, Button, Col, Container, ListGroup, Row } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import ExperienceListItem from "./ExperienceListItem";
+import { Plus } from "react-bootstrap-icons";
+import ModalAddExperience from "./ModalAddExperience";
+import { useState } from "react";
 
 
 const ModifyExperience = function () {
@@ -8,6 +11,8 @@ const ModifyExperience = function () {
   const experiences = useSelector((state) => {
     return state.experiences.data;
   });
+
+  const [addExperience, setAddExperience] = useState(false)
 
   const calcoloDate = function (dataInizio, dataFine) {
     const inizio = new Date(dataInizio);
@@ -33,6 +38,31 @@ const ModifyExperience = function () {
     <>
       <Container className="mt-3">
         <Row>
+          <Col xs={12}>
+            <Alert variant="light">
+            <Alert.Heading>Sezione Gestione Esperienze </Alert.Heading>
+            <hr></hr>
+            <p>In questa sezione potrai aggiungere , modificare, eliminare le tue esperienze lavorative</p>
+            </Alert>
+          </Col>
+        </Row>
+        <Row className="">
+          <Col xs={8}></Col>
+          <Col xs={4} className="text-end">
+          <Button
+                  variant="outline-primary"
+                  className=" align-text-top opacity-75"
+                  onClick={() => {setAddExperience(true)}}
+                >
+                  {/* TODO: AGGIUNGERE MODALE CON PUT PER CAMBIARNE IL CONTENUTO */}
+                  <Plus color="black" size={30}></Plus>AGGIUNGI UN&apos;ESPERIENZA
+                </Button></Col>
+                {
+                addExperience &&(
+          <ModalAddExperience setAddExperience={setAddExperience} />)
+                }
+        </Row>
+        <Row className="mt-3">
           <Col>
             <ListGroup>
                 {
