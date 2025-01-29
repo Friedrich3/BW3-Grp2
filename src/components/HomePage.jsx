597c6ccc754f4a8c6b24164d16cf79a5a390cfdb
migueljano <angel1999dtb@gmail.com>
2025-01-29 14:57:13 +0100

    modifiche home-scheltro

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -5,35 +5,35 @@ import { Link } from 'react-router-dom';
 const HomePage = () => {
 
     return (
-        <div className='bg-light'>
+        <div className='bg-white'>
             <h5>I giochi di oggi</h5>
             <Link to="https://www.linkedin.com/games/tango/" className='text-black text-decoration-none'>
                 <div className="content">
-                    <Image src='https://static.licdn.com/aero-v1/sc/h/gqavqd9gk6ja1s27lr1p2zz2' />
+                    <Image src='https://static.licdn.com/aero-v1/sc/h/gqavqd9gk6ja1s27lr1p2zz2' width={30} />
                     <div className="text-container">
-                        <p className='fw-bold fs-5 pt-3'>Tango</p>
+                        <p className='fw-bold fs-img pt-3'>Tango</p>
                         <p>Incorona ogni regione <ArrowBarRight className='ArrowBarRight' /></p>
                     </div>
                 </div>
             </Link>
             <Link to="https://www.linkedin.com/games/tango/" className="text-black text-decoration-none">
                 <div className="content">
-                    <Image src="https://static.licdn.com/aero-v1/sc/h/6uvsjtqx2j32uh1a803qygh5y" />
+                    <Image src="https://static.licdn.com/aero-v1/sc/h/6uvsjtqx2j32uh1a803qygh5y" width={30} />
                     <div className="text-container">
-                        <p className='fw-bold fs-5 pt-3'>Queens</p>
+                        <p className='fw-bold fs-img pt-3'>Queens</p>
                         <p>Incorona ogni regione <ArrowBarRight className='ArrowBarRight' /></p>
 
                     </div>
                 </div>
             </Link>
 
-            <button className='rounded borde-1 border-secondary p-3 bg-light BtnSeggerimenti'>
+            {/* <button className='rounded borde-1 border-secondary p-3 bg-light BtnSeggerimenti'>
                 <button className='rounded p-1'>SUGGERIMENTI</button>
                 <p className='fs-4'>
                     Prova Linkedin sull&apos;app <br />per Windows
                 </p>
 
-            </button>
+            </button> */}
 
         </div>
     );
