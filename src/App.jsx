89a9c3d6a83142f97eabd53cc44b387c4623b5a7
Jonathan130900 <jlb130900@gmail.com>
2025-01-29 15:15:35 +0100

    Finalizzato Sidebar News Homepage

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,19 @@ import Profile from "./components/Profile/Profile";
 import NavbarLinkedin from "./components/NavbarLinkedin";
 import NotFound from "./components/NotFound";
 import FooterLinkedin from "./components/FooterLinkedin";
-import { useDispatch, } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useEffect } from "react";
-import { getDataAction, } from "./redux/action";
+import { getDataAction } from "./redux/action";
 import ModifyExperience from "./components/Experience/ModifyExperience";
-import HomePage from "./components/HomePage";
+import HomeNews from "./components/Homepage/HomeNews";
 
 function App() {
-
-
   //EVENTUALMENTE DA SPOSTARE IN APP.Jsx PER OTTENERE LE INFO DEL PROFILO allo start della pagina
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getDataAction())
+    dispatch(getDataAction());
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  }, []);
 
   return (
     <BrowserRouter>
@@ -30,7 +28,7 @@ function App() {
 
         <Route path="*" element={<NotFound />} />
       </Routes>
-      <HomePage />
+      <HomeNews />
       <FooterLinkedin />
     </BrowserRouter>
   );
---
+++ src/components/Homepage/HomeNews.jsx
@@ -0,0 +1,112 @@
+import { Card } from "react-bootstrap";
+import { InfoSquareFill, ChevronCompactDown } from "react-bootstrap-icons";
+import { ArrowBarRight } from "react-bootstrap-icons";
+import Image from "react-bootstrap/Image";
+import { Link } from "react-router-dom";
+//dd
+const NewsCard = () => {
+  return (
+    <Card style={{ width: "18rem", marginBottom: "20px" }}>
+      <Card.Body>
+        <Card.Title className="d-flex justify-content-between align-items-center">
+          <span>In primo piano</span>
+          <InfoSquareFill className="text-dark fs-6" />
+        </Card.Title>
+
+        <Card.Subtitle className="my-3 text-muted">
+          a cura di LinkedIn Notizie
+        </Card.Subtitle>
+        <div className="news-item mb-2">
+          <span className="d-block fw-medium small">
+            L&apos;AI cinese Deepseek spaventa le Borse
+          </span>
+          <span className="text-muted small">
+            2 ore fa<span className="mx-1">•</span>1.733 lettori
+          </span>
+        </div>
+        <div className="news-item mb-2">
+          <strong className="d-block fw-medium small">
+            Mps vuole acquisire Mediobanca
+          </strong>
+          <span className="text-muted small">
+            3 ore fa<span className="mx-1">•</span>906 lettori
+          </span>
+        </div>
+        <div className="news-item mb-2">
+          <strong className="d-block fw-medium small">
+            Prove di settimana corta nella PA
+          </strong>
+          <span className="text-muted small">
+            19 ore fa<span className="mx-1">•</span>229 lettori
+          </span>
+        </div>
+        <div className="news-item mb-2">
+          <strong className="d-block fw-medium small">
+            Roccaraso: l&apos;overtourism è un problema
+          </strong>
+          <span className="text-muted small">
+            18 ore fa<span className="mx-1">•</span>171 lettori
+          </span>
+        </div>
+        <div className="news-item mb-2">
+          <strong className="d-block fw-medium small">
+            Obiettivi e traguardi dell&apos;Agenda Digitale...
+          </strong>
+          <span className="text-muted small">17 ore fa</span>
+        </div>
+        <button className="btn btn-outline-secondary border-0 rounded">
+          Vedi altro <ChevronCompactDown />
+        </button>
+      </Card.Body>
+      <div className="bg-white">
+        <h5>I giochi di oggi</h5>
+        <Link
+          to="https://www.linkedin.com/games/tango/"
+          className="text-black text-decoration-none"
+        >
+          <div className="content">
+            <Image
+              src="https://static.licdn.com/aero-v1/sc/h/gqavqd9gk6ja1s27lr1p2zz2"
+              width={30}
+            />
+            <div className="text-container">
+              <p className="fw-bold fs-img pt-3">Tango</p>
+              <p>
+                Incorona ogni regione{" "}
+                <ArrowBarRight className="ArrowBarRight" />
+              </p>
+            </div>
+          </div>
+        </Link>
+        <Link
+          to="https://www.linkedin.com/games/tango/"
+          className="text-black text-decoration-none"
+        >
+          <div className="content">
+            <Image
+              src="https://static.licdn.com/aero-v1/sc/h/6uvsjtqx2j32uh1a803qygh5y"
+              width={30}
+            />
+            <div className="text-container">
+              <p className="fw-bold fs-img pt-3">Queens</p>
+              <p>
+                Incorona ogni regione{" "}
+                <ArrowBarRight className="ArrowBarRight" />
+              </p>
+            </div>
+          </div>
+        </Link>
+
+        {/* <button className='rounded borde-1 border-secondary p-3 bg-light BtnSeggerimenti'>
+                <button className='rounded p-1'>SUGGERIMENTI</button>
+                <p className='fs-4'>
+                    Prova Linkedin sull&apos;app <br />per Windows
+                </p>
+
+            </button> */}
+      </div>
+    </Card>
+  );
+};
+
+export default NewsCard;
