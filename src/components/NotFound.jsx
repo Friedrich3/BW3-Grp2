765ec906178ae12b44815b315e0169507f4009a1
Federico Tonti <148573975+Friedrich3@users.noreply.github.com>
2025-01-30 16:28:51 +0100

    fixati link footer e aggiunta NOTFOUND

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,9 +1,23 @@
+import { Alert, Col, Container, Row } from "react-bootstrap"
+import FooterLinkedin from "./FooterLinkedin"
+
 const NotFound = function(){
 
     return(
         <>
-        
-
+            <Container style={{height:'50vh'}}>
+                <Row>
+                    <Col>
+                    <Alert variant="danger">
+        <Alert.Heading className="text-center">⚠️404 - PAGE NOT FOUND⚠️</Alert.Heading>
+        <p className="text-center">
+          Come gli unicorni anche queste pagine <span className="fw-bold">NON</span> esistono! O almeno non le abbiamo ancora inventate! 🥲
+        </p>
+      </Alert>
+                    </Col>
+                </Row>
+            </Container>  
+            <FooterLinkedin /> 
         </>
     )
 
